0411cc061a396bfd9b913b9756144552051b904c
Chinna <adikesavareddy6007@gmail.com>
2025-07-12 19:51:09 +0530

    second commit

---
+++ Adi_Katimi/src/Selenium100too/testingdomain_1.java
@@ -0,0 +1,13 @@
+package Selenium100too;
+
+public class testingdomain_1 {
+
+	public static void main(String[] args) {
+	
+		int a = 1;
+		String b = "chinna";
+		
+		System.out.println(b);
+	}
+
+}
